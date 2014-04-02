26522ef0682d89ec0d9940c4fefb7588484b9764
Prelang Builder <builder@prelang.com>
2014-04-02 15:25:44 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prelangtest::Application.config.secret_key_base = 'c75f81a64c5d5e812897fbc7eb4f35d809b113e5181a55cb0ce97e7517c425eb1b9c40ef20b9020f916a5f588788cf3063311feab104bbb6aec1dcd480bcc995'
